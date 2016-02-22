0fd860369d83e019eda63b00562bdea57dde056c
Miro Svrtan <miro@mirosvrtan.me>
2016-02-22 18:22:59 +0100

    Added ExternalService structure

---
+++ spec/External/ExternalServiceIdSpec.php
@@ -0,0 +1,24 @@
+<?php
+namespace spec\DevBoardLib\GithubCore\External;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ExternalServiceIdSpec extends ObjectBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType('DevBoardLib\GithubCore\External\ExternalServiceId');
+        $this->shouldHaveType('DevBoardLib\GithubCore\Identifier');
+    }
+
+    public function let()
+    {
+        $this->beConstructedWith(123);
+    }
+
+    public function it_has_to_string_conversion()
+    {
+        $this->__toString()->shouldReturn('123');
+    }
+}
